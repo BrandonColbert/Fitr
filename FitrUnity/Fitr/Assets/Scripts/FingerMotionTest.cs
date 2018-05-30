3be2602008bdfb857aa92d9fe41fbfdaa4b7a04c
Brandon Colbert <expertdash@gmail.com>
2018-05-29 21:08:26 -0400

    Created Unity Project to demo tracking

---
+++ FitrUnity/Fitr/Assets/Scripts/FingerMotionTest.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerMotionTest : MonoBehaviour {
+	[System.Serializable]
+	public struct FingerMotion {
+		public int fingerNumber;
+		public Vector3 rotation;
+		public float flex;
+	}
+
+	public FitrHand hand;
+	public bool autoTest = false;
+
+	public FingerMotion[] testers;
+
+	void FixedUpdate() {
+		for(int i = 0; i < testers.Length; i++) {
+			FingerMotion fm = testers[i];
+
+			FitrHand.Finger[] fingers = hand.GetFingers();
+			if(0 <= fm.fingerNumber && fm.fingerNumber < fingers.Length) {
+				if(autoTest) {
+					if(Random.Range(0, 2) == 1) {
+						float value = Random.Range(-10f, 10f);
+						
+						if(fm.flex > 90) {
+							value = -Mathf.Abs(value);
+						} else if(fm.flex < -20) {
+							value = Mathf.Abs(value);
+						}
+
+						fm.flex += value;
+					}
+
+					bool change = true;
+					Vector3 v = Vector3.zero;
+
+					switch(Random.Range(0, 8)) {
+						case 0:
+							v += Vector3.right;
+							break;
+						case 1:
+							v += Vector3.up;
+							break;
+						case 2:
+							v += Vector3.forward;
+							break;
+						default:
+							change = false;
+							break;
+					}
+					
+					if(change) {
+						Vector3 initial = fm.rotation;
+						fm.rotation += v * Random.Range(-6f, 6f);
+
+						float max = 30;
+						if(Mathf.Abs(fm.rotation.x) > max || Mathf.Abs(fm.rotation.y) > max || Mathf.Abs(fm.rotation.z) > max) {
+							fm.rotation = initial;
+						}
+					}
+
+					testers[i] = fm;
+				}
+
+				hand.MoveFinger(fingers[fm.fingerNumber], Quaternion.Euler(fm.rotation), fm.flex);
+			}
+		}
+	}
+}
---
+++ FitrUnity/Fitr/Assets/Scripts/FitrHand.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitrHand : MonoBehaviour {
+	[System.Serializable]
+	public struct Finger {
+		public bool connected;
+		public GameObject a, b, c;
+	}
+
+	public GameObject palm;
+	public Finger thumb, index, middle, ring, pinky;
+
+	void Update() {
+		foreach(Finger finger in GetFingers()) {
+			if(finger.a != null) finger.a.SetActive(finger.connected);
+			if(finger.b != null) finger.b.SetActive(finger.connected);
+			if(finger.c != null) finger.c.SetActive(finger.connected);
+		}
+	}
+
+	public void MoveFinger(Finger finger, Quaternion rotation, float flex) {
+		if(finger.connected) {
+			if(finger.c != null) {
+				Vector3 flexAngle = Vector3.back * flex;
+				finger.a.transform.localRotation = rotation;
+				finger.b.transform.localRotation = Quaternion.Euler(flexAngle);
+				finger.c.transform.localRotation = Quaternion.Euler(flexAngle * 1.2f);
+			} else {
+				finger.a.transform.localRotation = rotation;
+				finger.b.transform.localRotation = Quaternion.Euler(Vector3.left * flex);
+			}
+		}
+	}
+
+	public Finger[] GetFingers() {
+		return new Finger[] {
+			thumb, index, middle, ring, pinky
+		};
+	}
+}
